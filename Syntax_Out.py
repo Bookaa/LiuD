9f6448fdc4a9b4765282fd6bad53848aa725063f
Bookaa <taotaoliu@me.com>
2015-11-05 19:45:42 +0800

    03
    add Syntax_Out.py

---
+++ Syntax_Out.py
@@ -0,0 +1,52 @@
+# LiuTaoTao github.com/Bookaa/LiuD
+
+SynName = 'Out'
+
+ignores = {
+    'crlf'   : ( ' \t\n', [ '//.*', r'/\*(.|\n)*?\*/' ] ),
+    'wspace' : ( ' \t', [ r'/\*(.|\n)*?\*/' ] ),
+    'no'     : ( '', [] )
+}
+
+base_def = { 'NEWLINE' :    ('',    '\\n+'),
+             'NAME'    :    ('Name',  '[A-Za-z_][A-Za-z0-9_]*'),
+             'STRING'  :    ('String',  r"'(.|\n)*?'")
+             }
+
+s_tree = '''
+.syntax wspace
+
+stmt :: stmtone NEWLINE$
+    stmtone(s, vlst*) : NAME ':' item*
+
+    item :: ( Jiad(v1,vq?,v2) : X lnk? '^*' (String | X) )
+        | ( String : STRING )
+        | ( optgroup(vlst*) : '[' item+ ']' )
+        | ( itemd(vlst*) : '(' item+ ')*' )
+        | ( ident : ('+ident' | '-ident')$ )
+        | ( newline : 'NL' )
+        | ( lnk : '-' )
+        | ( Xlst : 'x*' )
+        | ( Xq : 'x?' )
+        | ( X : 'x' )
+
+
+Module(vlst*) : NEWLINE$? stmt* ENDMARKER$
+'''
+
+Out_self = '''
+Module : (x NL)*
+stmtone : x ':' x*
+Jiad : x x? '^*' x
+String : x
+optgroup : '[' x* ']'
+itemd : '(' x* ')*'
+ident : x
+newline : 'NL'
+lnk : '-'
+Xlst : 'x*'
+Xq : 'x?'
+X : 'x'
+'''
+
+s_sample = Out_self
